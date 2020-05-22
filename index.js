c279a60b79237b802153898a997c5330a52fe12e
Krishnabansalabc <krishna.bansal_da17@gla.ac.in>
2020-05-22 15:58:42 +0530

    move private information into dev.js

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://bansalkrishna:bansal@cluster0-fukdt.mongodb.net/test?retryWrites=true&w=majority',
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { User } = require('./model/user');
+
+const config = require('./config/key')
+
+mongoose.connect(config.mongoURI,
     {useNewUrlParser: true}).then(()=>console.log('DB connected'))
                             .catch(err=>console.error(err));
 
+
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+
 app.get('/',(req,res)=>{
-    res.send("Hello World!!")
-});
+    res.json({"hello ~": "Hi ~~ dakwfnaeub"})
+})
+
+
+app.post('/api/users/register',(req,res)=>{
+    const user = new User(req.body)
+
+    user.save((err,userData)=>{
+        if(err) return res.json({ success : false,err })
+        return res.status(200).json({
+        success:true
+    })
+})
+})
 
 app.listen(5000);
